3075a56065ef3626afae7bf478eb8c7789687853
Jesús Artiles <Jess Artiles@DESKTOP-A66ARKR>
2019-09-25 11:02:16 +0100

    Kata1 finish

--- NetBeansProjects/Kata1/src/kata1/Kata1.java
+++ NetBeansProjects/Kata1/src/kata1/Kata1.java
@@ -5,6 +5,8 @@
  */
 package kata1;
 
+import java.util.Date;
+
 /**
  *
  * @author Jesús Artiles
@@ -15,7 +17,10 @@ public class Kata1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Person p = new Person("Jesus", new Date(98,7,23));
+        System.out.println(p.getAge());
+                
+                
     }
     
 }
